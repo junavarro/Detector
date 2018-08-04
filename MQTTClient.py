976a28df34e04c43118b4803c2436584480f093b
Juan Esteban Navarro Camacho <jnavcamacho@gmail.com>
2018-08-04 10:31:30 -0600

    client connected to mqtt

--- MQTTClient.py
+++ MQTTClient.py
@@ -2,32 +2,50 @@ import paho.mqtt.client as mqtt
 import os, urlparse
 from threading import Thread
 
-# Define event callbacks
-def on_connect(client, userdata, flags, rc):
-    print("rc: " + str(rc))
 
-def on_message(client, obj, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-def on_publish(client, obj, mid):
-    print("mid: " + str(mid))
-
-def on_subscribe(client, obj, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-def on_log(client, obj, level, string):
-    print(string)
 
 
 class MQTTHelper (Thread):
+	# Define event callbacks
+	def on_connect(self,client, userdata, flags, rc):
+		print("rc: " + str(rc))
+
+	def on_message(self,client, obj, msg):
+		#print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+		self.lastImage = str(msg.payload)
+
+	def on_publish(self,client, obj, mid):
+		print("mid: " + str(mid))
+
+
+	def on_subscribe(self,client, obj, mid, granted_qos):
+		print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+	def on_log(self,client, obj, level, string):
+		print(string)	
+
+
+	def sendMessage (self,topic,message):
+		self.mqttc.publish(topic, message)
+
+	def run(self):
+		# Continue the network loop, exit when an error occurs
+		rc = 0
+		while rc == 0:
+			rc = self.mqttc.loop()
+			#print("rc: " + str(rc))
+
+
 	def __init__(self):
 		Thread.__init__(self)
+		
+		self.lastImage = None
 		self.mqttc = mqtt.Client()
 		# Assign event callbacks
-		self.mqttc.on_message = on_message
-		self.mqttc.on_connect = on_connect
-		self.mqttc.on_publish = on_publish
-		self.mqttc.on_subscribe = on_subscribe
+		self.mqttc.on_message = self.on_message
+		self.mqttc.on_connect = self.on_connect
+		self.mqttc.on_publish = self.on_publish
+		self.mqttc.on_subscribe = self.on_subscribe
 		# Uncomment to enable debug messages
 		#mqttc.on_log = on_log
 		# Parse CLOUDMQTT_URL (or fallback to localhost)
@@ -44,15 +62,10 @@ class MQTTHelper (Thread):
 		# Publish a message
 		#self.mqttc.publish(self.topic, "gotouch")
 
-	def sendMessage (self,topic,message):
-		self.mqttc.publish(topic, message)
+	def getLastImg(self):
+		return self.lastImage
 
-	def run(self):
-		# Continue the network loop, exit when an error occurs
-		rc = 0
-		while rc == 0:
-		    rc = self.mqttc.loop()
-		print("rc: " + str(rc))		
+			
 		
 #mqtt = MQTTHelper()
 #mqtt.start()
