f0b65948e971e8799f5add7b7a8f14c7075a791d
Juan Esteban Navarro Camacho <jnavcamacho@gmail.com>
2018-08-04 06:26:56 -0600

    lecture of data

--- Sensor.py
+++ Sensor.py
@@ -1,7 +1,28 @@
 from imp import load_source
 from os import system
+import matplotlib.pyplot as plt 
+import numpy as np
 WalabotAPI = load_source('WalabotAPI',
 '/usr/share/walabot/python/WalabotAPI.py')
+from threading import Thread
+
+
+class WalabotPlotter (Thread):
+	def __init__(self, matrix):
+		Thread.__init__(self)
+		self.matrix = matrix
+		self.plotter = plt.matshow(self.matrix)	
+
+
+	def updateGraph (self,matrix):
+		self.matrix = matrix
+		self.plotter.set_data(self.matrix)
+
+
+	def run(self):		
+		plt.show()
+
+
 
 
 
@@ -28,18 +49,30 @@ def connectWalabot():
 
 def calibrateWalabot():
 	print("Init Calibration")
+	WalabotAPI.StartCalibration()
 	appStatus, calibrationProcess = WalabotAPI.GetStatus()
-	while ( appStatus == WalabotAPI.STATUS_CALIBRATING ):
-		system("clear")
-		print("Calibrating Process",calibrationProcess)
-		appStatus, calibrationProcess = WalabotAPI.GetStatus()
+
+	#while ( appStatus == WalabotAPI.STATUS_CALIBRATING ):
+		#system("clear")
+		#print("Calibrating Process",calibrationProcess)
+		#appStatus, calibrationProcess = WalabotAPI.GetStatus()
 
 	appStatus, calibrationProcess = WalabotAPI.GetStatus()	
 	walabotStatus()
 
 def configureWalabot():
+	#Define the arena
+	
 	# Short-range, penetrative scanning inside dielectric materials such as walls.
 	WalabotAPI.SetProfile(WalabotAPI.PROF_SHORT_RANGE_IMAGING)# short range 
+	# Select scan arena
+	#             R             Phi          Theta
+	ARENA = [(10, 50, 4), (-60, 60, 5), (-15, 15, 5)]
+	# Set scan arena
+	WalabotAPI.SetArenaR(*ARENA[0])
+	WalabotAPI.SetArenaPhi(*ARENA[1])
+	WalabotAPI.SetArenaTheta(*ARENA[2])
+	WalabotAPI.SetThreshold(50)
 	#No need to remove weak signal. We are not focused on moving targets.
 	WalabotAPI.SetDynamicImageFilter(WalabotAPI.FILTER_TYPE_NONE)
 	print("Walabot Configured")
@@ -47,6 +80,25 @@ def configureWalabot():
 def startWalabot():
 	WalabotAPI.Start()
 
+def getData():
+	rasterImage, test1, test2, sliceDepth, power = WalabotAPI.GetRawImageSlice()
+	
+	while True:
+		WalabotAPI.Trigger()    
+		targets = WalabotAPI.GetImagingTargets()
+		print(WalabotAPI.GetRawImageSlice())
+		rasterImage, test1, test2, sliceDepth, power = WalabotAPI.GetRawImageSlice()
+		system("clear")
+		print(rasterImage)
+		print("Test 1: ", test1)
+		print("Test 2: ", test2)
+		print("Slide Depth: ",sliceDepth)
+		print("Power: ", power)
+		#walabotplot.updateGraph(rasterImage)
+
+		
+
+		
 
 
 
@@ -57,7 +109,11 @@ def startWalabot():
 WalabotAPI.Init() # Init the Wallabot.
 #Connect the Wallbot
 connectWalabot()
-#calibrate the walabot
-calibrateWalabot()
 # configure the walabot
 configureWalabot()
+#start
+startWalabot()
+#calibrate the walabot
+calibrateWalabot()
+#get the image
+getData()
